9f11472493308af9aba23a040369585e320e954d
Aleksandr_Chernuho <54019656+AleksCher1993@users.noreply.github.com>
2022-03-25 19:10:03 +0300

    0.0.1

---
+++ js/modules/timer.js
@@ -0,0 +1,17 @@
+export const timer = () => {
+  const timers = document.querySelectorAll(".timer__block");
+
+  function timerHandler() {
+    const date = new Date();
+    let hour = date.getHours() < 10 ? "0" + date.getHours() : date.getHours();
+    let min =
+      date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
+    let sec =
+      date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds();
+    timers[1].firstElementChild.innerHTML = hour;
+    timers[2].firstElementChild.innerHTML = min;
+    timers[3].firstElementChild.innerHTML = sec;
+  }
+
+  setInterval(timerHandler, 500);
+};
